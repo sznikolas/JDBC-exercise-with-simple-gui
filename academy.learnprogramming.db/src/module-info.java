b7ec15bc5aeacbdff554483ec6bc3eef49de7d82
sznikolas <szocs.nikolas@gmail.com>
2023-02-27 17:56:34 +0100

    JDBC (sqlite) exercise with simple GUI

---
+++ academy.learnprogramming.db/src/module-info.java
@@ -0,0 +1,7 @@
+module academy.learnprogramming.db {
+    exports academy.learnprogramming.db;
+
+    requires java.sql;
+    requires sqlite.jdbc;
+    requires transitive academy.learnprogramming.common;
+}
